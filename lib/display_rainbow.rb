dd799f2498923efc3a80eeb0377ec60b5559241f
Dalton Fant <daltonfant16@gmail.com>
2017-12-29 22:42:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,12 @@
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-puts "The first color on the list is: #{colors[0]}"
-puts "The second color on the list is: #{colors[1]}"
-puts "The third color on the list is: #{colors[2]}"
-puts "The fourth color on the list is: #{colors[3]}"
-puts "The fifth color on the list is: #{colors[4]}"
-puts "The sixth color on the list is: #{colors[5]}"
-puts "The seventh color on the list is: #{colors[6]}"
+def display_rainbow
+  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  puts "The first color on the list is: #{colors[0]}"
+  puts "The second color on the list is: #{colors[1]}"
+  puts "The third color on the list is: #{colors[2]}"
+  puts "The fourth color on the list is: #{colors[3]}"
+  puts "The fifth color on the list is: #{colors[4]}"
+  puts "The sixth color on the list is: #{colors[5]}"
+  puts "The seventh color on the list is: #{colors[6]}"
+end
+
+
